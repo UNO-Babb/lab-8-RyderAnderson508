97f53098067114ccffd823d8701bd27aedf9e1d2
RyderAnderson508 <ryderanderson@unomaha.edu>
2025-04-02 00:09:40 +0000

    sync

--- ProcessData.py
+++ ProcessData.py
@@ -1,7 +1,7 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name:Ryder Anderson
+#Date:04/01/2025
+#Assignment: Lab 08
 
 import random
 
@@ -12,12 +12,34 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    IDNUM = data[3]
+    student_id = makeID(first, last, IDNUM)
+    output = data[0]
+    outFile.write(output)
 
 
+    print(student_id)
+  
+ 
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
+
+def makeID(first, last, IDNUM):
+  print(first, last, IDNUM)
+  idlen = len(IDNUM)
+
+  while len(last) < 5:
+    last = last + "X"
+  id = first[0] + last + IDNUM[idlen - 3: ]
+  print(id)
+
+  return id 
 
 if __name__ == '__main__':
   main()
